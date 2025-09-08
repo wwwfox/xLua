06ab33fe54ed6ce764256d043fdf62af07a11a42
wwwfox <512808330@qq.com>
2025-09-08 20:51:33 +0800

    5.4.6

--- build/make_ohos_lua54.sh
+++ build/make_ohos_lua54.sh
@@ -13,7 +13,7 @@ export PATH=${NDK}/build-tools/cmake/bin:$PATH
 function build() {
     ABI=$1
     BUILD_PATH=build54.OHOS.${ABI}
-    cmake -H. -DLUA_VERSION=5.4.7 -DOHOS_STL=c++_shared -B${BUILD_PATH} -DOHOS_ARCH=${ABI} -DOHOS_PLATFORM=OHOS -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/ohos.toolchain.cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+    cmake -H. -DLUA_VERSION=5.4.6 -DOHOS_STL=c++_shared -B${BUILD_PATH} -DOHOS_ARCH=${ABI} -DOHOS_PLATFORM=OHOS -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/ohos.toolchain.cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
     cmake --build ${BUILD_PATH} --config Release
     mkdir -p plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/
     cp ${BUILD_PATH}/libxlua.so plugin_lua54/Plugins/OpenHarmony/libs/${ABI}/libxlua.so
